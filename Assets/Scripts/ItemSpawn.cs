681d7f6d16edc7c38212558538acee9d870008f2
Georgiy <gogapryst@gmail.com>
2022-07-02 09:21:46 +0300

    4

--- Assets/Scripts/ItemSpawn.cs
+++ Assets/Scripts/ItemSpawn.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ItemSpawn : MonoBehaviour
 {
     public GameObject[] items;
-    private bool isFull = false;
+    public bool isFull = false;
+    private float updateTime;
+    private float localTime;
     private void Start()
     {
         int r = Random.Range(0, 2);
@@ -14,6 +16,29 @@ public class ItemSpawn : MonoBehaviour
             int i = Random.Range(0, items.Length);
             Instantiate(items[i], transform.position, Quaternion.identity);
             isFull = true;
+        }
+        updateTime = Random.Range(10, 25);
+        localTime = updateTime;
+    }
+    private void Update()
+    {
+        if (!isFull)
+        {
+            localTime -= Time.deltaTime;
+        }
+        if (localTime < 0)
+        {
+            int i = Random.Range(0, items.Length);
+            Instantiate(items[i], transform.position, Quaternion.identity);
+            isFull = true;
+            localTime = updateTime;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isFull = false;
         }
     }
 }
